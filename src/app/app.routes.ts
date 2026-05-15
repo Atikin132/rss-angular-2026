3a299b16ab3e35cbbb2c697b962af2687fad3e35
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-15 21:21:06 +0300

    feat: RSA-3 add auth layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component/main-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component/auth-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
   },
 ];
