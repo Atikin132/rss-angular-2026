04134b90fda2b70c54fc8b099b6caa6dcbc37e47
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-15 20:35:48 +0300

    feat: RSA-3 add header

--- src/app/layouts/main-layout/main-layout.component/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
