1ca7e0718b61c168aa0d45b1dfbf82df5f0403e0
Artur Shvedenko <agtugchik13@gmail.com>
2026-05-17 14:01:28 +0300

    feat: RSA-5 add cart list and cart item components

--- src/app/pages/cart.page/cart.page.ts
+++ src/app/pages/cart.page/cart.page.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { CartListComponent } from '../../features/cart/components/cart-list.component/cart-list.component';
+import { MOCK_CART_ITEMS } from '../../features/cart/mock/mock-cart-items';
 
 @Component({
   selector: 'app-cart.page',
-  imports: [],
+  standalone: true,
+  imports: [CartListComponent],
   templateUrl: './cart.page.html',
   styleUrl: './cart.page.scss',
 })
-export class CartPage {}
+export class CartPage {
+  protected readonly cartItems = MOCK_CART_ITEMS;
+
+  onIncrease(id: string): void {
+    // eslint-disable-next-line no-console
+    console.log('increase', id);
+  }
+
+  onDecrease(id: string): void {
+    // eslint-disable-next-line no-console
+    console.log('decrease', id);
+  }
+
+  onRemove(id: string): void {
+    // eslint-disable-next-line no-console
+    console.log('remove', id);
+  }
+}
