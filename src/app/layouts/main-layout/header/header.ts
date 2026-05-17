2dba0d6aa1adcc8cd34cfad2bf0bfdaf48d5d89a
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-17 21:43:55 +0300

    refactor: RSA-7 refactor header using Angular Material

--- src/app/layouts/main-layout/header/header.ts
+++ src/app/layouts/main-layout/header/header.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  navItems = [
+    {
+      label: 'Main',
+      icon: 'home',
+      route: '/',
+    },
+    {
+      label: 'Catalog',
+      icon: 'menu_book',
+      route: '/catalog',
+    },
+    {
+      label: 'Cart',
+      icon: 'shopping_cart',
+      route: '/cart',
+    },
+    {
+      label: 'Profile',
+      icon: 'person',
+      route: '/profile',
+    },
+    {
+      label: 'About',
+      icon: 'info',
+      route: '/about',
+    },
+  ];
+}
