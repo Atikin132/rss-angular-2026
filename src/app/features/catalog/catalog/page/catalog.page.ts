7470a2c155fc1df2ef26a220e0ddc6dee622438b
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-24 12:01:04 +0300

    feat: RSA-24 add search

--- src/app/features/catalog/catalog/page/catalog.page.ts
+++ src/app/features/catalog/catalog/page/catalog.page.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { ProductCard } from '../product-card/product-card';
+import { Search } from '../search/search';
 
 @Component({
   selector: 'app-catalog.page',
-  imports: [ProductCard],
+  imports: [ProductCard, Search],
   templateUrl: './catalog.page.html',
   styleUrl: './catalog.page.scss',
 })
@@ -12,4 +13,20 @@ export class CatalogPage {
   private productsService = inject(ProductsService);
 
   products = this.productsService.products;
+
+  search = signal('');
+
+  searchedProducts = computed(() => {
+    const query = this.search().toLowerCase().trim();
+
+    if (!query) {
+      return this.products();
+    }
+
+    return this.products().filter((products) => products.name.toLowerCase().includes(query));
+  });
+
+  onSearchChange(value: string) {
+    this.search.set(value);
+  }
 }
