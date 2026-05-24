f4b939b6bdad95d458c28ce9ca026081c9efd363
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-24 13:39:59 +0300

    feat: RSA-24 add sort

--- src/app/features/catalog/catalog/page/catalog.page.ts
+++ src/app/features/catalog/catalog/page/catalog.page.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { ProductCard } from '../product-card/product-card';
 import { Search } from '../search/search';
+import { Sort, SortType } from '../sort/sort';
 
 @Component({
   selector: 'app-catalog.page',
-  imports: [ProductCard, Search],
+  imports: [ProductCard, Search, Sort],
   templateUrl: './catalog.page.html',
   styleUrl: './catalog.page.scss',
 })
@@ -15,18 +16,43 @@ export class CatalogPage {
   products = this.productsService.products;
 
   search = signal('');
+  sort = signal<SortType>('name-asc');
 
-  searchedProducts = computed(() => {
+  filteredAndSortedProducts = computed(() => {
     const query = this.search().toLowerCase().trim();
 
-    if (!query) {
-      return this.products();
+    let products = [...this.products()];
+
+    if (query) {
+      products = products.filter((product) => product.name.toLowerCase().includes(query));
     }
 
-    return this.products().filter((products) => products.name.toLowerCase().includes(query));
+    switch (this.sort()) {
+      case 'name-asc':
+        products.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+
+      case 'name-desc':
+        products.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+
+      case 'price-asc':
+        products.sort((a, b) => a.price - b.price);
+        break;
+
+      case 'price-desc':
+        products.sort((a, b) => b.price - a.price);
+        break;
+    }
+
+    return products;
   });
 
   onSearchChange(value: string) {
     this.search.set(value);
+  }
+
+  onSortChange(value: SortType) {
+    this.sort.set(value);
   }
 }
