65a44cfba3349fbc3e91115abd0f874f717ed521
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-07 12:19:03 +0300

    refactor: RSA-25 refactor product price

--- src/app/features/catalog/product-card/product-card.ts
+++ src/app/features/catalog/product-card/product-card.ts
@@ -1,21 +1,17 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CurrencyPipe } from '@angular/common';
 import { Product } from '../models/product.model';
 import { CartButton } from '../cart-button/cart-button';
 import { WishlistButton } from '../wishlist-button/wishlist-button';
 import { CompareButton } from '../compare-button/compare-button';
+import { ProductPrice } from '../product-price/product-price';
 
 @Component({
   selector: 'app-product-card',
-  imports: [RouterLink, CurrencyPipe, CartButton, CompareButton, WishlistButton],
+  imports: [RouterLink, CartButton, CompareButton, WishlistButton, ProductPrice],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
 export class ProductCard {
   product = input.required<Product>();
-
-  readonly actualPrice = computed(() => this.product().discountedPrice ?? this.product().price);
-
-  readonly hasDiscount = computed(() => this.product().discountedPrice != null);
 }
--- src/app/features/catalog/product-details/product-details.ts
+++ src/app/features/catalog/product-details/product-details.ts
@@ -1,22 +1,22 @@
-import { Component, computed, effect, inject, input, signal } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
 import { ProductsStore } from '../stores/products.store';
 import { ProductGallery } from './product-gallery/product-gallery';
-import { CurrencyPipe } from '@angular/common';
 import { Product } from '../models/product.model';
 import { ProductBreadcrumbs } from './product-breadcrumbs/product-breadcrumbs';
 import { CartButton } from '../cart-button/cart-button';
 import { WishlistButton } from '../wishlist-button/wishlist-button';
 import { CompareButton } from '../compare-button/compare-button';
+import { ProductPrice } from '../product-price/product-price';
 
 @Component({
   selector: 'app-product-details',
   imports: [
     ProductGallery,
-    CurrencyPipe,
     ProductBreadcrumbs,
     CartButton,
     WishlistButton,
     CompareButton,
+    ProductPrice,
   ],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
@@ -28,9 +28,6 @@ export class ProductDetailsPage {
 
   readonly product = signal<Product | null>(null);
   isLoading = this.store.loading;
-
-  readonly actualPrice = computed(() => this.product()?.discountedPrice ?? this.product()?.price);
-  readonly hasDiscount = computed(() => this.product()?.discountedPrice != null);
 
   constructor() {
     effect(async () => {
