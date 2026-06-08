d5347facaf8cf98f6dcfc560f2cdf0553bbd08f5
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-09 01:20:22 +0300

    feat: RSA-30 add environment with read-only client

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+
+  commercetools: {
+    projectKey: 'rss-angular-2026',
+
+    clientId: 'mjmEC5j9pQOKY51RM_ogYjwu',
+
+    clientSecret: '2_cYPifx07oSb7JYLCv1o1ot1Z_gBr4H',
+
+    authUrl: 'https://auth.europe-west1.gcp.commercetools.com',
+
+    apiUrl: 'https://api.europe-west1.gcp.commercetools.com',
+  },
+};
