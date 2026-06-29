f7e51eb0c5a99fcbb0d311ad32327884ec588dff
Kostev Oleg <olkostev@gmail.com>
2026-06-29 23:44:38 +0400

    refactor: RSA-55 use onpush change detection

--- src/app/features/profile/pages/profile/profile.component.ts
+++ src/app/features/profile/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -31,6 +31,7 @@ const namePattern = /^[\p{L}\s'-]+$/u;
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent {
   private readonly profileService = inject(ProfileService);
